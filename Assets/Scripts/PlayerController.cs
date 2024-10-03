f22c36b225e3b9cdc57d561811fc4a857dc64dfa
MaximumBloop <keepforeverstrong@gmail.com>
2024-10-02 21:29:11 -0500

    task1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+		{
+			rb.AddForce(Vector3.forward * speed - rb.velocity);
+		} else if (Input.GetKey(KeyCode.D))
+		{
+			rb.AddForce(Vector3.right * speed - rb.velocity);
+		} else if (Input.GetKey(KeyCode.S))
+		{
+			rb.AddForce(Vector3.back * speed - rb.velocity);
+		} else if (Input.GetKey(KeyCode.A))
+		{
+			rb.AddForce(Vector3.left * speed - rb.velocity);
+		} else {
+			rb.velocity = new Vector3(0, 0, 0);
+		}
+	}
+}
