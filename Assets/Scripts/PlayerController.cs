87f064cdef2e829656f6c14220a5ee64936c61fb
MaximumBloop <keepforeverstrong@gmail.com>
2024-10-02 22:06:03 -0500

    task4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	private int score = 0;
 	private Rigidbody rb;
 	// Use this for initialization
 	void Start () {
